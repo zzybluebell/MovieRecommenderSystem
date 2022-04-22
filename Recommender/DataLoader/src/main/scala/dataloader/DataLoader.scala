897d8e14748f2f889126a69b9a084a1c734f6011
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-04-23 03:19:16 +0800

    Add dataloader class

---
+++ Recommender/DataLoader/src/main/scala/dataloader/DataLoader.scala
@@ -0,0 +1,18 @@
+package dataloader
+
+// movies.csv
+// Each line of this file after the header row represents one movie, and has the following format:
+// movieId,title,genres
+// title includes the year of release in parentheses
+
+// ratings.csv
+// Each line of this file after the header row represents one rating of one movie by one user, and has the following format:
+// userId,movieId,rating,timestamp
+
+// tags.csv
+// Each line of this file after the header row represents one tag applied to one movie by one user, and has the following format:
+// userId,movieId,tag,timestamp
+
+object DataLoader {
+
+}
