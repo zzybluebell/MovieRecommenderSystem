bf0103bd94b32783bc4c05a2f39ed547027207ac
chenzaza <wxgych@gmail.com>
2022-04-25 01:42:01 +0800

    Add two more stats Recommenders

--- Recommender/statisticsRecommender/src/main/scala/statistics/StatisticsRecommender.scala
+++ Recommender/statisticsRecommender/src/main/scala/statistics/StatisticsRecommender.scala
@@ -12,6 +12,7 @@ object StatisticsRecommender {
   val METADATA_JSON ="Recommender/statisticsRecommender/src/main/resources/raw/metadata.json"
   val TAGS_JSON ="Recommender/statisticsRecommender/src/main/resources/raw/tags.json"
   val TAG_COUNT_JSON ="Recommender/statisticsRecommender/src/main/resources/raw/tag_count.json"
+  val RATINGS_JSON ="Recommender/statisticsRecommender/src/main/resources/raw/ratings.json"
   val MOVIES_CSV ="Recommender/statisticsRecommender/src/main/resources/raw/movies.csv"
   val OUT_DIR ="Recommender/statisticsRecommender/Output/"
   val FORMAT = ".csv"
@@ -61,11 +62,38 @@ object StatisticsRecommender {
     latest10DatesDF.coalesce(1).write.mode(SaveMode.Overwrite).csv(OUT_DIR + LATEST10_DATE)
     writeSingleCSV(LATEST10_DATE)
 
-    // received the most ratings - considered to be popular (TOP 10)
+    // MOST 10 ratings (count the number of users who gave a rating)
+    val countDF = spark.read.json(RATINGS_JSON).groupBy("item_id")
+      .count()
+      .orderBy(desc("count"))
+      .limit(10)
+    val most10RatingsDF = movieDF
+      .join(countDF, movieDF("mid") ===  countDF("item_id"),"inner")
+      .orderBy(desc("count"))
+      .select("count", "title")
+    most10RatingsDF.show()
+    most10RatingsDF.coalesce(1).write.mode(SaveMode.Overwrite).csv(OUT_DIR + MOST10_RATING)
+    writeSingleCSV(MOST10_RATING)
 
-
-    // highest rating with the SAME tag
-
+    // highest rating among the SAME tag
+    val tagsDF = spark.read.json(TAGS_JSON)
+    val tagCountDF = spark.read.json(TAG_COUNT_JSON)
+    val tagDF = tagCountDF
+      .join(tagsDF, tagsDF("id") === tagCountDF("tag_id"), "inner")
+      .select("item_id", "tag")
+    val tagRatingDF = tagDF
+      .join(movieDF, tagDF("item_id") ===  movieDF("mid"),"inner")
+      .select( "tag", "rating", "title")
+    val topRatingDF = tagRatingDF
+      .groupBy("tag").max("rating")
+      .withColumnRenamed("tag", "_tag")
+    val tagTopRatingDF = tagRatingDF
+      .join(topRatingDF, tagRatingDF("tag") === topRatingDF("_tag") && tagRatingDF("rating") ===  topRatingDF("max(rating)"),"inner")
+      .select("tag", "rating", "title")
+      .orderBy("tag")
+    tagTopRatingDF.show()
+    tagTopRatingDF.coalesce(1).write.mode(SaveMode.Overwrite).csv(OUT_DIR + TAG_TOP_RATING)
+    writeSingleCSV(TAG_TOP_RATING)
 
     spark.stop()
 
